19f00de871d255a3f817a65d83e13e4dea10895b
Jacque Evander <jacque.evander.ventura@gmail.com>
2022-08-24 14:18:29 -0400

    login logic updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import isLogged from "./Logic/isLogged";
+
+import { LoginView } from "./Views/Login/LoginView";
 import { Navbar } from "./Components/Navbar/Navbar";
-import { Routing } from "./Components/Routing/Routing";
+import { Routing } from "./Routing";
+import { RegisterView } from "./Views/Register/RegisterView";
+import { Login } from "./Components/LoginRegister/Login/Login";
 
 function App() {
-  return (
-    <>
-      <Navbar></Navbar>
+  const [token, setToken] = useState(false);
 
-      <Routing></Routing>
-    </>
+  return (
+    <React.Fragment>
+      <Routing />
+    </React.Fragment>
   );
 }
 
--- src/Logic/Login/reducerLogin.ts
+++ src/Logic/Login/reducerLogin.ts
@@ -1,13 +1,23 @@
+import { useNavigate } from "react-router-dom";
 import { actionLOGIN } from "./actionsLOGIN";
 
-const initialState = false;
+const initialState = [{
+    userId: '####',
+    firstName: 'NOT-LOGGED',
+    lastName: 'NOT-LOGGED',
+    isLogged: false,
+    lastConnection: Date.now()
+}]
 export default function reducerLogin(state=initialState, action:any){
     switch(action.type){
         case actionLOGIN.SIGN_IN:
-            return;
+            console.log('LOGGED INNN')
+            return {...action.payload, isLogged: true};
         
         case actionLOGIN.LOG_OUT:
-            return;
+            console.log('LOGGED OUT IM OUTTY')
+
+            return {...action.payload, isLogged: false};
 
         default:
             return state
