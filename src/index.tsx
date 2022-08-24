a1677328609e375dba828e5546cbc9557dc77d84
Jacque Evander <jacque.evander.ventura@gmail.com>
2022-08-24 14:20:45 -0400

    FontAwesomeIcons library updated

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,26 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import "./index.scss";
 
 import { BrowserRouter } from "react-router-dom";
+
 import { Provider } from "react-redux";
 import { rootStore } from "./Logic/rootStore";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+library.add(far, fas)
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <Provider store={rootStore}>
     <BrowserRouter>
-      <App />
+    <App></App>
     </BrowserRouter>
   </Provider>
 );
